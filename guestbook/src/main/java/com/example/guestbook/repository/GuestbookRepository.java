31eb46987302ce9b13473c42bc20e3ed2f446bfe
khj5606kh@gmail.com <khj5606kh@gmail.com>
2023-03-28 19:14:59 +0900

    등록 페이지와 처리 페이지 추가

--- guestbook/src/main/java/com/example/guestbook/repository/GuestbookRepository.java
+++ guestbook/src/main/java/com/example/guestbook/repository/GuestbookRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Component;
 
-//@Component
+@Component
 public interface GuestbookRepository extends JpaRepository<Guestbook, Long>, QuerydslPredicateExecutor<Guestbook> {
 }
--- guestbook/src/test/java/com/example/guestbook/repository/GuestbookRepositoryTests.java
+++ guestbook/src/test/java/com/example/guestbook/repository/GuestbookRepositoryTests.java
@@ -1,11 +1,19 @@
 package com.example.guestbook.repository;
 
 import com.example.guestbook.entity.Guestbook;
+import com.example.guestbook.entity.QGuestbook;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
 import java.util.stream.IntStream;
 
 @SpringBootTest
@@ -21,6 +29,64 @@ public class GuestbookRepositoryTests {
                     .writer("user" + (i%10))
                     .build();
             System.out.println(guestbookRepository.save(guestbook));
+        });
+    }
+
+    @Test
+    public void updateTest(){
+        Optional<Guestbook> result = guestbookRepository.findById(10L);
+        if(result.isPresent()){
+            Guestbook guestbook = result.get();
+            guestbook.changeTitle("Changed Title ..... ");
+            guestbook.changeContent("Changed Content ..... ");
+            guestbookRepository.save(guestbook);
+        }
+    }
+
+    //단일 항목 검색 : 제목에 '1'이라는 글자가 있는 엔티티들 검색
+    //noresultexception error : 검색결과가 하나도 안나왔을 때 발생
+    //    @Transactional(readOnly = true)
+    @Test
+    public void testQuery1(){
+        Pageable pageable = PageRequest.of(0,10, Sort.by("gno").descending());
+        QGuestbook qGuestbook = QGuestbook.guestbook;
+        String keyword = "1";
+        BooleanBuilder builder = new BooleanBuilder();
+        BooleanExpression expression = qGuestbook.title.contains(keyword);
+        builder.and(expression);
+        /*
+        Page<Guestbook> result = guestbookRepository.findAll(builder, pageable);
+        result.stream().forEach(guestbook -> {
+            System.out.println(guestbook);
+        });
+*/
+        try{
+            Page<Guestbook> result = guestbookRepository.findAll(builder, pageable);
+            result.stream().forEach(guestbook -> {
+                System.out.println(guestbook);
+            });
+        }catch (Exception e){
+            e.printStackTrace();
+            System.out.println(e);
+        }
+    }
+
+    // 같은 에러
+    @Test
+    public void testQuery2() {
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("gno").descending());
+        QGuestbook qGuestbook = QGuestbook.guestbook;
+        String keyword = "1";
+        BooleanBuilder builder = new BooleanBuilder();
+        BooleanExpression exTitle = qGuestbook.title.contains(keyword);
+        BooleanExpression exContent = qGuestbook.content.contains(keyword);
+        BooleanExpression exAll = exTitle.or(exContent);
+        builder.and(exAll);
+        builder.and(qGuestbook.gno.gt(0L));
+
+        Page<Guestbook> result = guestbookRepository.findAll(builder, pageable);
+        result.stream().forEach(guestbook -> {
+            System.out.println(guestbook);
         });
     }
 }
